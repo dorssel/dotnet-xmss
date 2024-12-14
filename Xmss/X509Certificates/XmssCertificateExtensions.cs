2389101efb26068d6280f35a61107aba8bbc5dc3
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2024-12-14 19:50:10 +0100

    Add import of public keys

---
+++ Xmss/X509Certificates/XmssCertificateExtensions.cs
@@ -0,0 +1,32 @@
+﻿// SPDX-FileCopyrightText: 2024 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace Dorssel.Security.Cryptography.X509Certificates;
+
+public static class XmssCertificateExtensions
+{
+    public static Xmss? GetXmssPublicKey(this X509Certificate2 certificate)
+    {
+        ArgumentNullException.ThrowIfNull(certificate);
+
+        if (certificate.PublicKey.Oid.Value != Xmss.IdAlgXmssHashsig.Value)
+        {
+            return null;
+        }
+        var xmss = new Xmss();
+        try
+        {
+            xmss.ImportCertificatePublicKey(certificate);
+            var result = xmss;
+            xmss = null;
+            return result;
+        }
+        finally
+        {
+            xmss?.Dispose();
+        }
+    }
+}
